6b721d790ede488de43f189159adf08fe61123a4
Rogério Zolly <rogeriozolly603@gmail.com>
2026-06-27 18:19:53 -0300

    refactoring dim_customers and fct_orders; adding stripe payments data model to stg

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,27 @@
+WITH orders AS (
+    SELECT * FROM {{ ref('stg_jaffle_shop__orders') }}
+),
+
+payments AS (
+    SELECT * FROM {{ ref('stg_stripe__payments') }}
+),
+
+order_payments AS (
+    SELECT 
+        order_id, 
+        SUM(CASE WHEN payments.status = 'success' THEN payments.amount ELSE 0 END) as amount 
+    FROM payments
+    GROUP BY 1
+),
+
+final AS (
+    SELECT 
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+    FROM orders
+    LEFT JOIN order_payments USING (order_id)
+)
+
+SELECT * FROM final
---
+++ models/staging/stg_stripe__payments.sql
@@ -0,0 +1,9 @@
+select
+    ID as payment_id,
+    ORDERID as order_id,
+    PAYMENTMETHOD as payment_method,
+    STATUS as status,
+    AMOUNT / 100 as amount,
+    CREATED as created_at
+
+from `jaffleshop-499601.stripe.payments`
